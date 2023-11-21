3618c6e1d122df85d1be29e42f46c9569d8c480b
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-11-21 12:28:06 +0530

    chore: remove HttpLoggingInterceptor

--- app/src/main/java/com/alexmercerind/audire/api/shazam/ShazamRetrofitInstance.kt
+++ app/src/main/java/com/alexmercerind/audire/api/shazam/ShazamRetrofitInstance.kt
@@ -1,7 +1,5 @@
 package com.alexmercerind.audire.api.shazam
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,15 +9,9 @@ abstract class ShazamRetrofitInstance {
         private const val BASE_URL = "https://amp.shazam.com/"
 
         private val instance: Retrofit by lazy {
-            val interceptor = HttpLoggingInterceptor()
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .build()
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
                 .build()
         }
 
