df3597ec09c2d8a716e510c9ba839caaa4453739
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-01-01 20:15:03 +0530

    feat: SettingsActivity backup (import & export)

--- app/src/main/java/com/alexmercerind/audire/ui/SettingsViewModel.kt
+++ app/src/main/java/com/alexmercerind/audire/ui/SettingsViewModel.kt
@@ -1,17 +1,30 @@
 package com.alexmercerind.audire.ui
 
 import android.app.Application
+import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
+import com.alexmercerind.audire.repository.HistoryRepository
+import com.alexmercerind.audire.repository.ImportExportRepository
 import com.alexmercerind.audire.repository.SettingsRepository
+import kotlinx.coroutines.flow.first
 
 class SettingsViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository = SettingsRepository(application)
+    private val historyRepository = HistoryRepository(application)
+    private val settingsRepository = SettingsRepository(application)
+    private val importExportRepository = ImportExportRepository(application)
 
-    val theme = repository.theme
+    val theme = settingsRepository.theme
 
-    val systemColorScheme = repository.systemColorScheme
+    val systemColorScheme = settingsRepository.systemColorScheme
 
-    fun setTheme(value: String) = repository.setTheme(value)
+    fun setTheme(value: String) = settingsRepository.setTheme(value)
 
-    fun setSystemColorScheme(value: Boolean) = repository.setSystemColorScheme(value)
+    fun setSystemColorScheme(value: Boolean) = settingsRepository.setSystemColorScheme(value)
+
+    suspend fun import(uri: Uri) = importExportRepository.import(uri)
+
+    suspend fun export(uri: Uri) = importExportRepository.export(
+        uri,
+        historyRepository.getAll().first()
+    )
 }
